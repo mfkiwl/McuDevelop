251ff0446b37d8befa3d3f92d6585452232cdc1e
buggy <buggy@zhtlab.dip.jp>
2019-02-09 17:54:34 +0900

    adxl345.[ch]  adxl345 access

---
+++ examples/IMU4P/subcpu/firm/src/adxl345.c
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2018,2019 zhtlab
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to permit
+ * persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#define _ADXL345_C_
+
+#include        <stdint.h>
+
+#include        "main.h"
+#include        "imu.h"
+
+#include        "adxl345.h"
+
+
+/************************************************************
+ * Adxl345Probe()
+ */
+int
+Adxl345Probe(int unit)
+{
+  int           result = IMU_ERRNO_UNKNOWN;
+  uint8_t       c;
+
+  ImuGetValueStandard(unit, ADXL345_REG_DEVID, &c, 1);
+  if(c == DEVID_ADXL345) {
+    result = IMU_ERRNO_SUCCESS;
+  }
+
+  return result;
+}
+
+
+/************************************************************
+ * Adxl345Init()
+ */
+int
+Adxl345Init(int unit)
+{
+  int           result = IMU_ERRNO_UNKNOWN;
+  uint8_t       reg, val;
+
+  /* power ctl   off first */
+  reg = ADXL345_POWER_CTL;
+  val = 0x00;
+  ImuSetValueStandard(unit, reg, val);
+
+  SystemWaitCounter(2);
+
+  /* bw rate */
+  reg = ADXL345_BW_RATE;
+  val = BW_RATE_RATE_100HZ;
+  ImuSetValueStandard(unit, reg, val);
+
+  /* SPI 4lines */
+  reg = ADXL345_REG_DATA_FORMAT;
+  val = DATA_FORMAT_RANGE_PM16G;
+  ImuSetValueStandard(unit, reg, val);
+
+  /* interrupt  data ready, map int1, */
+  reg = ADXL345_REG_INT_MAP;
+  val = 0x00;           /* mapt to INT1 */
+  ImuSetValueStandard(unit, reg, val);
+
+  /* enable interrupt */
+  reg = ADXL345_REG_INT_ENABLE;
+  val = INT_ENABLE_DATA_READY;
+  ImuSetValueStandard(unit, reg, val);
+
+  /* power ctl */
+  reg = ADXL345_POWER_CTL;
+  val = POWER_CTL_MEASURE_EN;
+  ImuSetValueStandard(unit, reg, val);
+
+  return result;
+}
+
+
+#define XL      ((ADXL345_REG_DATAX_LOW )-(ADXL345_REG_DATAX_LOW))
+#define XH      ((ADXL345_REG_DATAX_HIGH)-(ADXL345_REG_DATAX_LOW))
+#define YL      ((ADXL345_REG_DATAY_LOW )-(ADXL345_REG_DATAX_LOW))
+#define YH      ((ADXL345_REG_DATAY_HIGH)-(ADXL345_REG_DATAX_LOW))
+#define ZL      ((ADXL345_REG_DATAZ_LOW )-(ADXL345_REG_DATAX_LOW))
+#define ZH      ((ADXL345_REG_DATAZ_HIGH)-(ADXL345_REG_DATAX_LOW))
+int
+Adxl345ReadValue(int unit, imuValue_t *p)
+{
+  uint8_t       cmd;
+  uint8_t       buf[32];
+  uint16_t      temp;
+
+  if(!p) goto fail;
+
+  ImuGetValueStandard(unit, ADXL345_MB | ADXL345_REG_DATAX_LOW, buf, 6);
+
+  p->acc.x = (buf[XH] << 8) | buf[XL];
+  p->acc.y = (buf[YH] << 8) | buf[YL];
+  p->acc.z = (buf[ZH] << 8) | buf[ZL];
+  p->gyro.x = 0;
+  p->gyro.y = 0;
+  p->gyro.z = 0;
+
+  p->ts = 0;
+  p->temp4x = 0;
+
+fail:
+  return 0;
+}
---
+++ examples/IMU4P/subcpu/firm/src/adxl345.h
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2018,2019 zhtlab
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to permit
+ * persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef _ADXL345_H_
+#define _ADXL345_H_
+
+#define ADXL345_READ_WRITE_MASK  (1<<7)
+#define ADXL345_READ             (1<<7)
+#define ADXL345_WRITE            (0<<7)
+#define ADXL345_MB               (1<<6)
+
+#define ADXL345_REG_DEVID            0x00
+#define         DEVID_ADXL345                   0xe5
+
+#define ADXL345_BW_RATE              0x2c
+#define         BW_RATE_RATE_SHIFT              (0)
+#define         BW_RATE_RATE_MASK               (0xf<<(BW_RATE_RATE_SHIFT))
+#define         BW_RATE_RATE_0_1HZ              ( 0<<(BW_RATE_RATE_SHIFT))
+#define         BW_RATE_RATE_100HZ              (10<<(BW_RATE_RATE_SHIFT))
+#define         BW_RATE_RATE_200HZ              (11<<(BW_RATE_RATE_SHIFT))
+#define         BW_RATE_RATE_400HZ              (12<<(BW_RATE_RATE_SHIFT))
+#define         BW_RATE_RATE_800HZ              (13<<(BW_RATE_RATE_SHIFT))
+#define         BW_RATE_RATE_1600HZ             (14<<(BW_RATE_RATE_SHIFT))
+#define         BW_RATE_RATE_3200HZ             (15<<(BW_RATE_RATE_SHIFT))
+
+#define ADXL345_POWER_CTL            0x2d
+#define         POWER_CTL_MEASURE_SHIFT         (3)
+#define         POWER_CTL_MEASURE_MASK          (1<<(POWER_CTL_MEASURE_SHIFT))
+#define         POWER_CTL_MEASURE_DIS           (0<<(POWER_CTL_MEASURE_SHIFT))
+#define         POWER_CTL_MEASURE_EN            (1<<(POWER_CTL_MEASURE_SHIFT))
+#define         POWER_CTL_SLEEP                 (1<<2)
+#define         POWER_CTL_WAKEUP_SHIFT          (0)
+#define         POWER_CTL_WAKEUP_MASK           (3<<(POWER_CTL_WAKEUP_SHIFT))
+#define         POWER_CTL_WAKEUP_8HZ            (0<<(POWER_CTL_WAKEUP_SHIFT))
+#define         POWER_CTL_WAKEUP_4HZ            (1<<(POWER_CTL_WAKEUP_SHIFT))
+#define         POWER_CTL_WAKEUP_2HZ            (2<<(POWER_CTL_WAKEUP_SHIFT))
+#define         POWER_CTL_WAKEUP_1HZ            (3<<(POWER_CTL_WAKEUP_SHIFT))
+
+#define ADXL345_REG_INT_ENABLE       0x2e
+#define         INT_ENABLE_DATA_READY           (1<<7)
+#define ADXL345_REG_INT_MAP          0x2f
+#define ADXL345_REG_INT_SOURCE       0x30
+
+#define ADXL345_REG_DATA_FORMAT      0x31
+#define         DATA_FORMAT_JUSTIFY_SHIFT       (2)
+#define         DATA_FORMAT_JUSTIFY_MASK        (1<<(DATA_FORMAT_JUSTIFY_SHIFT))
+#define         DATA_FORMAT_JUSTIFY_LSB         (0<<(DATA_FORMAT_JUSTIFY_SHIFT))
+#define         DATA_FORMAT_JUSTIFY_MSB         (1<<(DATA_FORMAT_JUSTIFY_SHIFT))
+#define         DATA_FORMAT_RANGE_SHIFT        (0)
+#define         DATA_FORMAT_RANGE_MASK         (3<<(DATA_FORMAT_RANGE_SHIFT))
+#define         DATA_FORMAT_RANGE_PM2G         (0<<(DATA_FORMAT_RANGE_SHIFT))
+#define         DATA_FORMAT_RANGE_PM4G         (1<<(DATA_FORMAT_RANGE_SHIFT))
+#define         DATA_FORMAT_RANGE_PM8G         (2<<(DATA_FORMAT_RANGE_SHIFT))
+#define         DATA_FORMAT_RANGE_PM16G        (3<<(DATA_FORMAT_RANGE_SHIFT))
+#define ADXL345_REG_DATAX_LOW        0x32
+#define ADXL345_REG_DATAX_HIGH       0x33
+#define ADXL345_REG_DATAY_LOW        0x34
+#define ADXL345_REG_DATAY_HIGH       0x35
+#define ADXL345_REG_DATAZ_LOW        0x36
+#define ADXL345_REG_DATAZ_HIGH       0x37
+
+
+int     Adxl345Probe(int unit);
+int     Adxl345Init(int unit);
+int     Adxl345ReadValue(int unit, imuValue_t *p);
+void    Adxl345SetValue(int unit, int reg, uint8_t val);
+void    Adxl345GetValue(int unit, int reg, uint8_t *ptr, int size);
+
+#ifdef  _ADXL345_H_
+#endif
+
+#endif
